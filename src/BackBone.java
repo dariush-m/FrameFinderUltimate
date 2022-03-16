7e20793040dd84e0d8397d3e9e21942e409954b3
dariush <dariush.mirkarimi@gmail.com>
2022-03-16 10:35:33 +0100

    BackBone skeleton added

--- src/BackBone.java
+++ src/BackBone.java
@@ -1,2 +1,13 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class BackBone {
+    static List<ORF> ORFs;
+    static HashMap<String, String> parameters;
+    static String fileContent;
+    static String nucSequence;
+
+    private void extractSequence(){
+        // Sets the value of this.nucSequence to the sequence extracted out of this.fileContent.
+    }
 }
