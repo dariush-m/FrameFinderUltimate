01c37dd698223d3626902d7999fca26cb1d902c5
dariush <dariush.mirkarimi@gmail.com>
2022-03-16 10:30:00 +0100

    BLAST skeleton added

--- src/BLAST.java
+++ src/BLAST.java
@@ -1,2 +1,9 @@
+import java.util.List;
+
 public class BLAST {
+    protected List<String> blastORFs(List<ORF> ORFs){
+        // Run the BLAST search and return the results in XML format
+        // Maybe make a separate thread.
+        return List.of("");
+    }
 }
