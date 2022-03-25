57e9547f449a953eb4d2634f9fe292dc1fad49be
dariush <dariush.mirkarimi@gmail.com>
2022-03-25 12:11:05 +0100

    GUI is functional

--- src/FrameFinderUltimate/BackBone.java
+++ src/FrameFinderUltimate/BackBone.java
@@ -14,20 +14,33 @@ public class BackBone {
     static Sequence nucSequence;
     static String inputFilePath;
     static String saveFilePath;
+    static GUI window = new GUI();
+
+
+
+    protected static void analyse() throws IOException{
+        extractSequence();
+        nucSequence.FindORFs();
+        var x = nucSequence.getORFs();
+        System.out.println(x);
+
+    }
 
     protected static void extractSequence() throws IOException {
-        inputFilePath = FileHandling.selectInputFile();
         // Sets the this.inputFilePath to the chosen file path.
         fileContent = FileHandling.retrieveData();
         // Retrieves the sequences from the chosen input file
+        nucSequence = new Sequence(fileContent);
     }
 
     protected static void setInput(){
         inputFilePath = FileHandling.selectInputFile();
+        window.inputField.setText(inputFilePath);
     }
 
     protected static void setOutput(){
         saveFilePath = FileHandling.selectOutputFile();
+        window.outputField.setText(saveFilePath);
     }
 
     protected static void setFileContent() throws IOException {
@@ -42,4 +55,10 @@ public class BackBone {
     protected static void setParameters(){
         parameters.put("test", "test");
     }
+
+
+    public static void main(String[] args) {
+
+    }
 }
+
